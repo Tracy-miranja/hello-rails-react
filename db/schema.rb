8172c5016e0a6b2fcf863565e9d04d6067aef993
Tracy-miranja <barazatracy44@gmail.com>
2023-08-06 22:51:21 +0300

    Add page for display

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_073803) do
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
-    t.string "greeting"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
